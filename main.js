e7f82060c9fd527e8d58db85a4bf359758b4818c
Moses-TM <mosesmagar97@gmail.com>
2025-07-01 05:03:41 +0000

    add click-and-drag functionality

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//CREATE 16 * 16 GRID USING FLEX
 const gridContainer = document.getElementById("grid-container")
 let gridSize = 16
 
@@ -9,6 +10,28 @@ for(let i = 0; i < gridSize; i++){
     }
 }
 
-// Check number of items in container
+// CHECK NUMBER OF ITEMS IN CONTAINER
 // const divCount = gridContainer.querySelectorAll('div')
 // console.log(divCount.length)
+
+//ON CLICK + DRAG CHANGES COLOR PERMANENTLY
+const gridItem = document.querySelectorAll('.grid-item')
+let isMouseDown = false
+gridItem.forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        if (isMouseDown){
+            item.classList.add('active')
+        }
+    })
+
+    item.addEventListener('mousedown', () => {
+        item.classList.add('active')
+    })
+})
+
+//TRACK MOUSE STATE GLOBALLY
+document.addEventListener('mousedown', () => isMouseDown = true)
+document.addEventListener('mouseup', () => isMouseDown = false)
+
+//PREVENT DRAG SELECTION(CLICK + DRAG)
+document.addEventListener('dragstart' , (e) => e.preventDefault())
